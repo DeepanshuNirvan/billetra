2f16fc1a6a87447ebc8d81a4ea2272ed4b4fa204
deepanshu <ddnirvan@gmail.com>
2026-06-16 23:29:32 +0530

    prefix changed

--- backend/internal/utils/invoice.go
+++ backend/internal/utils/invoice.go
@@ -2,11 +2,9 @@ package utils
 
 import (
 	"fmt"
-	"time"
 )
 
 // GenerateInvoiceNumber creates an invoice number in the format PREFIX-YYYY-NNNN
 func GenerateInvoiceNumber(prefix string, counter int) string {
-	year := time.Now().Year()
-	return fmt.Sprintf("%s-%d-%04d", prefix, year, counter)
+	return fmt.Sprintf("%s/%04d", prefix, counter)
 }
